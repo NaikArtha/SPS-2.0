0720d9d65dcb04c2f0db5da7f16664020d8cbef8
Aayush Anand <53388828+aadhgoa@users.noreply.github.com>
2021-06-14 06:58:49 +0000

    Tried to complete the game and excludes scores

--- Code1.py
+++ Code1.py
@@ -19,22 +19,54 @@ for i in range(1, number_of_players + 1):
     countCode = 0
     print("Player", i, "enter the code for the following:")
     Codes[countPlayer, countCode] = getpass.getpass("Stone: ")
-    os.system('cls')
+   
     countCode += 1
     print("Player", i, "enter the code for the following:")
     Codes[countPlayer, countCode] = getpass.getpass("Paper: ")
     countCode += 1
-    os.system('cls')
+    
     print("Player", i, "enter the code for the following:")
     Codes[countPlayer, countCode] = getpass.getpass("Scissors: ")
     countPlayer += 1
-    os.system('cls')
-    print("")
-    print("")
-
-for count in Codes:
-    for i in count:
-        print(i, end=" ")
-    print(" \n ")
     
+    print("")
+    print("")
 
+    
+player1 = {
+  Codes[0][0] : 'Stone',
+  Codes[0][1] : 'Paper',
+  Codes[0][2] : 'Scissor',
+}
+player2 = {
+  Codes[1][0] : 'Stone',
+  Codes[1][1] : 'Paper',
+  Codes[1][2] : 'Scissor',
+}
+
+
+#print(player1[player1Code])
+#print(player2[player2Code])
+
+
+
+RESULTS = {
+    ("Stone", "Scissor"): "Stone beats scissors, Player 1 wins! ",
+    ("Paper", "Stone"): "Paper beats stone, tough luck! Player 1 wins! ",
+    ("Scissor", "Paper"): "Scissors me timbers! Scissors cuts paper! Player 1 wins",
+    ("Scissor", "Stone"): "Stone beats scissors,  Player 2 wins!",
+    ("Stone","Paper"): "Paper beats stone, Player 2 wins!",
+    ("Paper", "Scissor"): "Scissors me timbers! Scissors cuts paper! Player 2 wins"
+}
+
+def game_play(input1, input2):
+  
+  print(RESULTS.get((input1, input2), "It's a draw!"))
+
+
+while True:
+  player1Code = int(input("Enter your code: "))
+  player2Code = int(input("Enter your code: "))
+  game_play(player1[player1Code], player2[player2Code])
+  if input("Do you want to quit? Else press Enter to continue").lower() in {"y", "yes"}:
+    break
